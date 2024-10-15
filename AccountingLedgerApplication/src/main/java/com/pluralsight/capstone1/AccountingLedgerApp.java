80dc3ddf0425181ca52f573a4440b917138084c5
Grecia-Torres <grecia.atorres@gmail.com>
2024-10-15 10:57:37 -0700

    created transactions.csv file

---
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/capstone1/AccountingLedgerApp.java
@@ -0,0 +1,11 @@
+package com.pluralsight.capstone1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AccountingLedgerApp {
+    public static void main(String[] args) {
+
+    }
+}
