39cc940ce0f23945041f40f1ff544c58b2e66bf3
Grecia-Torres <grecia.atorres@gmail.com>
2024-10-15 13:24:11 -0700

    Completed initial part of homeScreen

--- AccountingLedgerApplication/src/main/java/com/pluralsight/capstone1/AccountingLedgerApp.java
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/capstone1/AccountingLedgerApp.java
@@ -2,10 +2,44 @@ package com.pluralsight.capstone1;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class AccountingLedgerApp {
     public static void main(String[] args) {
+        homeScreen();
+    }
 
+    public static void homeScreen() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("                 HOME   ");
+            System.out.println("Please choose an option from the following: ");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit\n");
+            String options = scanner.nextLine();
+
+            switch (options) {
+                case "D":
+                    System.out.println("D) Add Deposit: ");
+                    break;
+                case "P":
+                    System.out.println("P) Make Payment(Debit): ");
+                    break;
+                case "L":
+                    System.out.println("L) Ledger: ");
+                    break;
+                case "X":
+                    System.out.println("X) Exit: ");
+                    scanner.close();
+                    return;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/capstone1/Ledger.java
@@ -0,0 +1,4 @@
+package com.pluralsight.capstone1;
+
+public class Ledger {
+}
