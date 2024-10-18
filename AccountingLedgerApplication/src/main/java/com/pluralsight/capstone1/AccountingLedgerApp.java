66e852db2791922be3397f47680816bcd170f54b
Grecia-Torres <grecia.atorres@gmail.com>
2024-10-17 17:45:53 -0700

    updated payment time format

--- AccountingLedgerApplication/src/main/java/com/pluralsight/capstone1/AccountingLedgerApp.java
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/capstone1/AccountingLedgerApp.java
@@ -60,12 +60,12 @@ public class AccountingLedgerApp {
 
         LocalDate date = LocalDate.now();
         LocalTime time = LocalTime.now();
-        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
-        String formatTime = time.format(timeFormat);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String timeFormatted = time.format(formatter);
         String transactionsFile = "transactions.csv";
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(transactionsFile, true))) {
-            writer.write(date + "|" + timeFormat + "|" + description + "|" + vendor + "|" + amount);
+            writer.write(date + "|" + timeFormatted + "|" + description + "|" + vendor + "|" + amount);
             writer.newLine();
             System.out.println("Your deposit has been accepted.");
         } catch (IOException e) {
@@ -88,10 +88,12 @@ public class AccountingLedgerApp {
 
         LocalDate date = LocalDate.now();
         LocalTime time = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String timeFormatted = time.format(formatter);
         String transactionsFile = "transactions.csv";
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(transactionsFile, true))) {
-            writer.write(date + "|" + time + "|" + description + "|" + vendor + "|" + amount);
+            writer.write(date + "|" + timeFormatted + "|" + description + "|" + vendor + "|" + amount);
             writer.newLine();
             System.out.println("Your payment has been accepted.");
 
